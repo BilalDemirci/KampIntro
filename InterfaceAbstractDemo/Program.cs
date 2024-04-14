4083d6e792621760d1010bfc341b0765a4c891dd
Bilal Demirci <dmrc45500@icloud.com>
2024-04-14 14:10:05 +0300

    14.04.2024

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+customerManager.Save(new Customer
+{
+    DateOfBirth = new DateTime(1999,3,25),
+    FirstName = "Bilal",
+    LastName = "Demirci",
+     NationalityId = "61075121328"
+});
+
+Console.ReadLine();
+
