fecefb61d9fcdb8ae7cc27a18ac0fcfc9ac2fae0
Bilal Demirci <dmrc45500@icloud.com>
2024-04-22 13:18:01 +0300

    22.04.2024

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using EntityFrameworkDemo;
+
+//GetAll();
+GetProductsByCategory(1);
+
+Console.ReadLine();
+
+static void GetAll()
+{
+    NorthwindContext northwindContext = new NorthwindContext();
+    foreach (var product in northwindContext.Products)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+static void GetProductsByCategory(int categoryId)
+{
+    NorthwindContext northwindContext = new NorthwindContext();
+
+    var result = northwindContext.Products.Where(p=>p.CategoryId == categoryId);
+
+    foreach (var product in result)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -9,36 +9,37 @@ List<Product> products = new List<Product>
 {
     new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 GB Ram",UnitPrice=25000, UnitInStock=5},
     new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB Ram",UnitPrice=15000, UnitInStock=3},
-    new Product{ProductId=3,CategoryId=1,ProductName="HP Laptop",QuantityPerUnit="8 GB Ram",UnitPrice=1000, UnitInStock=2},
+    new Product{ProductId=3,CategoryId=1,ProductName="HP Laptop",QuantityPerUnit="8 GB Ram",UnitPrice=10000, UnitInStock=2},
     new Product{ProductId=4,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 GB Ram",UnitPrice=5000, UnitInStock=15},
     new Product{ProductId=5,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="4 GB Ram",UnitPrice=7500, UnitInStock=0},
 
 };
 
-Console.WriteLine("ALGORİTMİK --------------------------------------");
+//Test(products);
+//Console.WriteLine("--------------------------------------");
 
-foreach (Product product in products)
+//GetProducts(products);
+//Console.WriteLine("--------------------------------------");
+
+//GetProductsLinq(products);
+//AnyTest(products);
+//FindTest(products);
+//FindAllTest(products);
+//AscDescTest(products);
+//ClassicLinqTest(products);
+
+var result = from p in products
+             join c in categories
+             on p.CategoryId equals c.CategoryId
+             where p.UnitPrice>5000
+             orderby p.UnitPrice descending
+             select new ProductDto {ProductId = p.ProductId,CategoryName=c.CategoryName, ProductName=p.ProductName,UnitPrice=p.UnitPrice};
+
+foreach (var productDto in result)
 {
-    if (product.UnitPrice > 7500 && product.UnitInStock>3)
-    {
-        Console.WriteLine(product.ProductName);
-    }
+    Console.WriteLine("{0} --- {1}", productDto.ProductName,productDto.CategoryName);
 }
 
-Console.WriteLine("LİNQ --------------------------------------------");
-var result = products.Where(p => p.UnitPrice >= 5000 && p.UnitInStock > 2);
-foreach (Product product in result)
-{
-    Console.WriteLine(product.ProductName);
-}
-
-Console.WriteLine("--------------------------------------");
-
-GetProducts(products);
-
-Console.WriteLine("--------------------------------------");
-
-GetProductsLinq(products);
 
 Console.ReadLine();
 
@@ -60,6 +61,74 @@ static List<Product> GetProductsLinq(List<Product> products)
     return products.Where(p => p.UnitPrice >= 5000 && p.UnitInStock > 2).ToList();
 }
 
+static void Test(List<Product> products)
+{
+    Console.WriteLine("ALGORİTMİK --------------------------------------");
+
+    foreach (Product product in products)
+    {
+        if (product.UnitPrice > 7500 && product.UnitInStock > 3)
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+
+    Console.WriteLine("LİNQ --------------------------------------------");
+    var result = products.Where(p => p.UnitPrice >= 5000 && p.UnitInStock > 2);
+    foreach (Product product in result)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+static void AnyTest(List<Product> products)
+{
+    var result = products.Any(p => p.ProductName == "Dell Laptop");
+    Console.WriteLine(result);
+}
+
+static void FindTest(List<Product> products)
+{
+    var result = products.Find(p => p.ProductId == 4);
+    Console.WriteLine(result);
+    Console.WriteLine(result.ProductName);
+}
+
+static void FindAllTest(List<Product> products)
+{
+    var result = products.FindAll(p => p.ProductName.Contains("top"));
+    Console.WriteLine(result);
+}
+
+static void AscDescTest(List<Product> products)
+{
+    var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice);
+
+    foreach (Product product in result)
+    {
+        Console.WriteLine(product.ProductName);
+    }
+}
+
+//static void ClassicLinqTest(List<Product> products)
+//{
+//    var result = from p in products
+//                 where p.UnitPrice > 6000
+//                 orderby p.UnitPrice descending, p.ProductName ascending
+//                 select new ProductDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+//    foreach (Product product in result)
+//    {
+//        Console.WriteLine(product.ProductName);
+//    }
+//}
+
+class ProductDto
+{
+    public int ProductId { get; set; }
+    public string CategoryName { get; set; }
+    public string ProductName { get; set; }
+    public decimal UnitPrice { get; set; }
+}
 
 class Product
 {
